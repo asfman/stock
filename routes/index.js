97db21ed322e475d652b3d59df3b4f62f8cdd2c3
asfman <asfman@gmail.com>
2015-12-07 12:50:04 +0800

    add test.ejs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Hello Vue.js' });
 });
 
+router.get('/test', function(req, res) {
+  res.render('test', { title: 'Hello Vue.js' });
+});
+
 module.exports = router;
