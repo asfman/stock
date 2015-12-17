d602fd4d317ef1a277dcd0946eeb350b81f51c90
asfman <asfman@gmail.com>
2015-12-17 10:27:47 +0800

    show stock name

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -37,7 +37,7 @@ function formatResult(result) {
 	var lastPrice = ret[2];
 	var percent = (ret[3] - ret[2]) * 100 /ret[2];
 	//ret[0] + " " +
-	return parseFloat(ret[3],10).toFixed(2) + " " + percent.toFixed(2) 
+	return ret[0] + " " + parseFloat(ret[3],10).toFixed(2) + " " + percent.toFixed(2) 
 	+ " " + parseFloat(ret[2],10).toFixed(2)
 	+ " " + parseFloat(ret[4],10).toFixed(2) 
 	+ " " + parseFloat(ret[5],10).toFixed(2); 	
