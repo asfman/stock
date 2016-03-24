d47f39779d64dd8e0e7faeecccb1cbbd8a712c51
asfman <asfman@gmail.com>
2016-03-24 16:11:38 +0800

    add command page

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/test', function(req, res) {
   res.render('test', { title: 'Hello Vue.js' });
 });
 
+router.get('/asfman', function(req, res){
+	res.render('asfman', { title: 'Hello asfman' });
+});
+
 router.get('/wjd', function(req, res) {
 	var url = "http://iguba.eastmoney.com/action.aspx?action=getuserreply&uid=2923094077285486&rnd=" + new Date().getTime();
 	var fetch = require('node-fetch');
